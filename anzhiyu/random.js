b35c0212bd0fce751b4ddd8e8a224406d0e3e019
userKangkang <hwqqz678@sjtu.edu.cn>
2025-03-05 00:14:29 +0800

    Site updated: 2025-03-05 00:14:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/03/hello-world/","2025/03/03/cse-filesystem/","2025/03/04/cse-rpc/","2025/03/04/cse-dfs/","2025/03/04/cse-kv/","2025/03/04/cse-tx/","2025/03/04/cse-paxos/"];function toRandomPost(){
+var posts=["2025/03/03/hello-world/","2025/03/03/cse-filesystem/","2025/03/04/cse-rpc/","2025/03/04/cse-dfs/","2025/03/04/cse-kv/","2025/03/04/cse-tx/","2025/03/04/cse-paxos/","2025/03/05/cse-network/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
