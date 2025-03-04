060b25f07a81388af3e709ca277481e4a5ee906d
userKangkang <hwqqz678@sjtu.edu.cn>
2025-03-04 22:04:15 +0800

    Site updated: 2025-03-04 22:04:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/03/hello-world/","2025/03/03/cse-filesystem/"];function toRandomPost(){
+var posts=["2025/03/03/hello-world/","2025/03/03/cse-filesystem/","2025/03/04/cse-rpc/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
